73beb18864c0117ce8c2d55dfc78c63eb2d4a7c0
dimzcraft <151798683+dimz-craft@users.noreply.github.com>
2024-05-14 12:20:46 +0700

    Create blank.jsx

---
+++ src/pages/blank.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Blank = () => {
+  return (
+    <div>
+      <div style={{ marginTop: "100px" }}></div>
+      <center>
+        <h1>Blank</h1>
+        <div>The page you entered doesn't exist :(</div>
+        <div>If this page is a download link, please report it to the discord or YouTube server @Dimz_craft</div>
+      </center>
+    </div>
+  );
+};
+
+export default Blank;
