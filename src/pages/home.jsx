83b407dff84bceff7cc4a5436a0da7a5fb673bc0
dimzcraft <151798683+dimz-craft@users.noreply.github.com>
2024-05-14 19:12:55 +0700

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <h1>Nothing</h1>
       <div>
         I'm currently creating this website, <br />
-        download my texture pack or addons on <a href="https://modbay.org/user/Dimzcraft/">Modbay</a> and <a href="https://mcpedl.com/user/dimzcraft/">Mcpedl</a>
+        download my texture pack or addons on <a href="https://modbay.org/user/Dimzcraft/" className="link">Modbay</a> and <a href="https://mcpedl.com/user/dimzcraft/" className="link">Mcpedl</a>
       </div>
       </center>
     </div>
