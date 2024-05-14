120e827171c18746341b867b1201acb9ee50dd45
dimzcraft <151798683+dimz-craft@users.noreply.github.com>
2024-05-14 09:41:31 +0700

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
-      <div style={{ marginTop: "100px" }}>ADS</div>
+      <div style={{ marginTop: "100px" }}></div>
       <center>
       <h1>Nothing</h1>
       <div>emmm, did you press cancel download? check the link history and enter the download link again :)</div>
