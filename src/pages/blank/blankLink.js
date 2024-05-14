cae915bd9172b941ad1296f8d2be9baa9f67dceb
dimzcraft <151798683+dimz-craft@users.noreply.github.com>
2024-05-14 21:54:44 +0700

    Update blankLink.js

--- src/pages/blank/blankLink.js
+++ src/pages/blank/blankLink.js
@@ -3,14 +3,16 @@ const blankLink = [
   {
     id: "*",
     blankTitle: "🤥",
-    blankMessage: "The page you entered doesn't exist :( \nIf this page is a download link, please report it to the discord or YouTube server @Dimz_craft",
+    blankMessage: "The page you entered doesn't exist :(
+      If this page is a download link, please report it to the discord or YouTube server @Dimz_craft",
   },
 
   // cancel download
   {
     id: "/download-cancel",
     blankTitle: "🤨",
-    blankMessage: "why did you cancel the download? \nIf you accidentally cancel the download, please open the history and enter the download link again",
+    blankMessage: "why did you cancel the download?
+      If you accidentally cancel the download, please open the history and enter the download link again",
   },
 ];
 
