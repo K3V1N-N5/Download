dcc726ad091de4727f5f93ed865a8e4aa3d2994b
dimzcraft <151798683+dimz-craft@users.noreply.github.com>
2024-05-14 21:10:41 +0700

    Update blankPage.jsx

--- src/pages/blankPage.jsx
+++ src/pages/blankPage.jsx
@@ -1,8 +1,29 @@
+import React from 'react';
+
+// link
 const blankLink = [
   // default 
   {
     id: "*",
-    title: "🗿",
-    message: "yamero",
+    blankTitle: "🗿",
+    blankMessage: "daijobu",
   },
-]
+];
+
+export default blankLink;
+
+// page
+const BlankPage = ({ props }) => {
+
+  return (
+    <div>
+      <div style={{ marginTop: '100px' }}></div>
+      <center>
+        <h1>{props.blankTitle}</h1>
+        <div>{props.blankMessage}</div>
+      </center>
+    </div>
+  );
+};
+
+export default BlankPage;
