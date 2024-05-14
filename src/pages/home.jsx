71b366e9e63e2485418660c807cef85b8d2bb61e
dimzcraft <151798683+dimz-craft@users.noreply.github.com>
2024-05-14 19:06:49 +0700

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <center>
       <h1>Nothing</h1>
       <div>
-        I'm currently making a website :), <br />
+        I'm currently creating this website, <br />
         download my texture pack or addons on <a href="https://modbay.org/user/Dimzcraft/">Modbay</a> and <a href="https://mcpedl.com/user/dimzcraft/">Mcpedl</a>
       </div>
       </center>
