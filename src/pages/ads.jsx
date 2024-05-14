dc25c9feffc07894b272d8d964d81871fbfa02e9
dimzcraft <151798683+dimz-craft@users.noreply.github.com>
2024-05-14 11:56:50 +0700

    Update ads.jsx

--- src/pages/ads.jsx
+++ src/pages/ads.jsx
@@ -1,18 +1,26 @@
 import React, { useEffect, useRef } from "react";
 
 const allowedBrowsers = ["Chrome", "Edg", "Firefox", "Vivaldi"];
+const redirectLink = "https://youtube.com/@dimz_craft?si=ttPfsF59JZijxpw0";
+
 const Ads = ({ props }) => {
+  const userAgent = navigator.userAgent;
+  const isAllowedBrowser = allowedBrowsers.some((browser) =>
+    userAgent.includes(browser)
+  );
 
-    const userAgent = navigator.userAgent;
-    const isAllowedBrowser = allowedBrowsers.some((browser) =>
-      userAgent.includes(browser)
-    );
-
+  useEffect(() => {
     if (!isAllowedBrowser) {
       window.alert(
         "If download error please use browser: Chrome, Edge, FireFox, Vivaldi"
       );
+    } else {
+      const redirectTimer = setTimeout(() => {
+        window.location.href = redirectLink;
+      }, 6000);
+      return () => clearTimeout(redirectTimer);
     }
+  }, []);
 
   return (
     <div>
@@ -20,8 +28,9 @@ const Ads = ({ props }) => {
         <div>
           <div style={{ marginTop: "100px" }}></div>
           Downloading file... <br />
-          If download error please use browser: Chrome, Edge,FireFox, Vivaldi <br />
-          If you don't get the download file, please chat to the discord server or comment on YouTube @dimz_craft
+          If download error please use browser: Chrome, Edge,FireFox, Vivaldi<br />
+          If you don't get the download file, please chat to the discord server
+          or comment on YouTube @dimz_craft
         </div>
       </center>
     </div>
