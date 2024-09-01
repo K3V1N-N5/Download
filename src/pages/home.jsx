2eb0248db1acfa4aba6fa5bef5272ddb30cf27ea
K3V1N <52075293+K3V1N-N5@users.noreply.github.com>
2024-09-02 01:01:48 +0700

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,16 +3,30 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>
-      <div style={{ marginTop: "100px" }}></div>
-      <center>
-      <h1>Nothing</h1>
-      <div>
-        I'm currently working on this website, <br />
-        download my texture pack or addons on <a href="https://modbay.org/user/Dimzcraft/" className="link">Modbay</a> and <a href="https://mcpedl.com/user/dimzcraft/" className="link">Mcpedl</a>
+    <div class="page-wrapper chiller-theme toggled">
+  <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
+    <i class="fas fa-bars"></i>
+  </a>
+  <!-- Button  -->
+  
+  <nav id="sidebar" class="sidebar-wrapper">
+    <div class="sidebar-content">
+      <div class="sidebar-brand">
+        <a href="#">pro sidebar</a>
+        <div id="close-sidebar"><i class="fas fa-times"></i></div>
       </div>
-      </center>
-    </div>
+      <!-- sidebar-brand -->
+      <div class="sidebar-header">
+        <div class="user-pic" style="color:#fff;">
+          <i class="fa fa-user-circle fa-4x" aria-hidden="true"></i>
+        </div>
+        <div class="user-info">
+          <span class="user-name"> <strong>Joe Chien</strong></span>
+          <span class="user-role">Administrator</span>
+          <span class="user-status"><i class="fa fa-circle"></i> <span>Online</span></span>
+        </div>
+      </div>
+      <!-- sidebar-header -->
   );
 };
 
