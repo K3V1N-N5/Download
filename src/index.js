cfbbe94bf6d479ed20d425a202a14e1ba418b38c
AlvinQID <alvinqid@gmail.com>
2024-09-02 11:58:02 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Sidebar from '../alvinqid/sidebar';
+import Sidebar from './alvinqid/sidebar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
