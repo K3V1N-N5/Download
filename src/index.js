1096d239be290ffcdd15f021c5fabc6eeebf0f89
AlvinQID <alvinqid@gmail.com>
2024-09-02 13:02:32 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Sidebar from './alvinqid/sidebar';
 
 ReactDOM.render(
   <React.StrictMode>
