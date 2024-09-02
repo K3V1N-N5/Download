4053170d3f5d4dd3de2e7ed6b4a39d69ac4ca051
K3V1N <52075293+K3V1N-N5@users.noreply.github.com>
2024-09-02 16:13:59 +0700

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,45 +1,19 @@
-
-import React, { useState } from 'react';
-import './App.css'; // Pastikan Anda membuat file CSS yang sesuai
-import { NavLink } from 'react-router-dom';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-  const [navOpen, setNavOpen] = useState(false);
-
-  const openNav = () => {
-    setNavOpen(true);
-  };
-
-  const closeNav = () => {
-    setNavOpen(false);
-  };
-
   return (
     <div>
-      {/* Header */}
-      <header className="header">
-        <button onClick={openNav}>☰ Open Navigation</button>
-      </header>
-
-      {/* Navigation Bar */}
-      <div className={`sidenav ${navOpen ? 'open' : ''}`}>
-        <button className="closebtn" onClick={closeNav}>
-          &times;
-        </button>
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Services</a>
-        <a href="#">Contact</a>
+      <div style={{ marginTop: "100px" }}></div>
+      <center>
+      <h1>Nothing</h1>
+      <div>
+        I'm currently working on this website, <br />
+        download my texture pack or addons on <a href="https://modbay.org/user/Dimzcraft/" className="link">Modbay</a> and <a href="https://mcpedl.com/user/dimzcraft/" className="link">Mcpedl</a>
       </div>
-
-      {/* Main Content */}
-      <div className="main">
-        <h2>Example Navigation Bar</h2>
-        <p>Click the button above to open the navigation bar.</p>
-      </div>
+      </center>
     </div>
   );
-}
+};
 
 export default Home;
